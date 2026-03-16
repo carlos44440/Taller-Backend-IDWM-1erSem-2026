96baf98c6da582db42858f060128f8d20f8d7cdc
CarlosArauco8556 <129557200+CarlosArauco8556@users.noreply.github.com>
2026-03-16 15:00:50 -0300

    feat(data): add and configure DataContext

--- Program.cs
+++ Program.cs
@@ -1,8 +1,20 @@
+using DotNetEnv;
+using Microsoft.EntityFrameworkCore;
 using Serilog;
+using TiendaUCN.src.Infrastructure.Data;
+
+Env.Load();
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
+
+#region Database Configuration
+Log.Information("Configurando base de datos SQLite");
+string connectionStringDB = Environment.GetEnvironmentVariable("DATA_BASE_URL") ?? throw new ArgumentNullException("DataBase name cannot be null or empty");
+builder.Services.AddDbContext<DataContext>(options => options.UseSqlite(connectionStringDB));
+#endregion
+
 
 #region Logging Configuration
 builder.Host.UseSerilog((context, services, configuration) => configuration
