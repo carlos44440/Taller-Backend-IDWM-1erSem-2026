8d49df29c674d43908a3d1cab5de308a857b3d04
CarlosArauco8556 <129557200+CarlosArauco8556@users.noreply.github.com>
2026-03-25 12:55:41 -0300

    refactor(models): change user model

---
+++ src/Infrastructure/Data/Migrations/20260325155139_SecondMigration.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+
+#nullable disable
+
+namespace TiendaUCN.src.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SecondMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "VerificationCode",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "VerificationCodeExpiry",
+                table: "Users");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.CreateTable(
+                name: "VerificationCode",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Code = table.Column<string>(type: "TEXT", nullable: false),
+                    Expiry = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    FailedAttempts = table.Column<int>(type: "INTEGER", nullable: false),
+                    DateToResend = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UserId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VerificationCode", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_VerificationCode_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VerificationCode_UserId",
+                table: "VerificationCode",
+                column: "UserId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "VerificationCode");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Users");
+
+            migrationBuilder.AddColumn<string>(
+                name: "VerificationCode",
+                table: "Users",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "VerificationCodeExpiry",
+                table: "Users",
+                type: "TEXT",
+                nullable: true);
+        }
+    }
+}
