ec3d2a54692812c6fdd005137edd208d83403458
CarlosArauco8556 <129557200+CarlosArauco8556@users.noreply.github.com>
2026-04-10 22:17:55 -0400

    feat(dto): add cart dtos

--- src/Application/Services/Implements/CartService.cs
+++ src/Application/Services/Implements/CartService.cs
@@ -1,11 +1,22 @@
+using TiendaUCN.src.Application.DTOs.CartDTO;
 using TiendaUCN.src.Application.Services.Interfaces;
+using TiendaUCN.src.Infrastructure.Repositories.Interfaces;
 
 namespace TiendaUCN.src.Application.Services.Implements
 {
     public class CartService : ICartService
     {
-        public CartService()
+        private readonly ICartRepository _cartRepository;
+        private readonly IProductRepository _productRepository;
+        public CartService(ICartRepository cartRepository, IProductRepository productRepository)
         {
+            _cartRepository = cartRepository;
+            _productRepository = productRepository;
         }
+
+        // public async Task<CartDTO> CreateOrGetCartAsync(string buyerId, int? userId)
+        // {
+
+        // }
     }
 }
--- src/Application/Services/Interfaces/ICartService.cs
+++ src/Application/Services/Interfaces/ICartService.cs
@@ -1,7 +1,9 @@
+using TiendaUCN.src.Application.DTOs.CartDTO;
+
 namespace TiendaUCN.src.Application.Services.Interfaces
 {
     public interface ICartService
     {
-
+        // Task<CartDTO> CreateOrGetCartAsync(string buyerId, int? userId);
     }
 }
